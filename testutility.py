b56ecc7c95d00b1732f3538da55590b56f203fbc
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-04-12 16:09:11 +0530

    Part2

--- testutility.py
+++ testutility.py
@@ -45,10 +45,35 @@ def col_header_val(df, table_config):
       logging.info(f'expected columns: {expected_col}')
       return 0
 
+def humanbytes(B):
+   'Return the given bytes as a human friendly KB, MB, GB, or TB string'
+   B = float(B)
+   KB = float(1024)
+   MB = float(KB ** 2) # 1,048,576
+   GB = float(KB ** 3) # 1,073,741,824
+   TB = float(KB ** 4) # 1,099,511,627,776
+
+   if B < KB:
+      return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+   elif KB <= B < MB:
+      return '{0:.2f} KB'.format(B/KB)
+   elif MB <= B < GB:
+      return '{0:.2f} MB'.format(B/MB)
+   elif GB <= B < TB:
+      return '{0:.2f} GB'.format(B/GB)
+   elif TB <= B:
+      return '{0:.2f} TB'.format(B/TB)
+
 def stats(df, config_data):
   if col_header_val(df, config_data) == 1:
     col_names = list(df.columns)
     no_of_cols = df.shape[1]
     no_of_rows = df.shape[0]
-    statistics = f"\nNo. of Columns: {no_of_cols} \nNo. of Rows: {no_of_rows} \nColumn Names: {col_names}"
+    size = df.memory_usage(deep=True)
+    file_size = humanbytes(size)
+    statistics = f"\nNo. of Columns: {no_of_cols} \nNo. of Rows: {no_of_rows} \nColumn Names: {col_names} \nFile Size: {file_size}"
     print(statistics)
+
+def save_gz(df):
+  df.to_csv('compressed_train.gz', sep='|', compression='gzip')
+  print("File saved in gz format with pipe separator")
